4f669a21543ca1c1e49364482446644f3dc37566
lucas <stempest@163.com>
2025-04-15 23:16:32 +0800

    chore(all): React Template Building Step 1 1. 初始化 npm，添加 react 核心库。 2. 添加 webpack 构建工具及核心插件。 3. 添加 babel 编译器及核心插件。 4. 添加 typescript 以及相关类型库。 5. 添加 git、babel、webpack、typescript配置文件。 6. 根据 cra-template 迁移 index.htm l及 tsconfig 配置。 7. 添加 react 入口文件。

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>this is web app with react lib</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.tsx",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts|jsx|js)$/,
+        use: "babel-loader",
+        exclude: path.resolve(__dirname, "./node_modules"),
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./public/index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./dist"),
+    },
+    compress: true,
+    port: 8081,
+    open: true,
+  },
+};
