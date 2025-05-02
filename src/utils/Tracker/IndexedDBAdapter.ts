2fec0392017260f17c481179afbf3c15fbfaf8d4
lucas <stempest@163.com>
2025-05-02 19:06:18 +0800

    chore(IndexDB): add IndexDB methods

--- src/utils/Tracker/IndexedDBAdapter.ts
+++ src/utils/Tracker/IndexedDBAdapter.ts
@@ -1,33 +1,84 @@
 export default class IndexedDBAdapter {
   private db: IDBDatabase | null = null;
 
-  constructor(
-    private dbName: string,
-    private storeName: string
-  ) {
+  constructor(private dbName: string) {
     this.dbName = dbName;
-    this.storeName = storeName;
   }
 
-  private async initDB(): Promise<IDBDatabase> {
+  private async initDB(
+    storeName: string | string[],
+    indexes: string | string[]
+  ): Promise<IDBDatabase> {
     if (this.db) return this.db;
 
     return new Promise<IDBDatabase>((resolve, reject) => {
-      const dbRequest = indexedDB.open(this.dbName, 1);
+      const request = indexedDB.open(this.dbName, 1);
 
-      dbRequest.onupgradeneeded = (event) => {
+      request.onupgradeneeded = (event) => {
         const db = (event.target as IDBOpenDBRequest).result;
-        if (!db.objectStoreNames.contains(this.storeName)) {
-          db.createObjectStore(this.storeName, { autoIncrement: true });
+
+        if (Array.isArray(storeName)) {
+          storeName.forEach((storeItem) => {
+            if (!db.objectStoreNames.contains(storeItem))
+              db.createObjectStore(storeItem, { autoIncrement: true });
+          });
+        } else {
+          if (!db.objectStoreNames.contains(storeName))
+            db.createObjectStore(storeName, { autoIncrement: true });
         }
       };
 
-      dbRequest.onsuccess = (event) => {
+      request.onsuccess = (event) => {
         this.db = (event.target as IDBOpenDBRequest).result;
         resolve(this.db);
       };
 
-      dbRequest.onerror = reject;
+      request.onerror = reject;
+    });
+  }
+
+  private async addData(
+    storeName: string,
+    data: Record<string, any> | Record<string, any>[]
+  ): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      // 校验DB对象
+      if (!this.db) {
+        reject(new Error("Database is undefined."));
+        return;
+      }
+
+      // 校验数据格式
+      if (Array.isArray(data)) {
+        if (data.length === 0) {
+          reject(new Error("Cannot add empty array."));
+          return;
+        }
+      } else {
+        if (Object.keys(data).length === 0) {
+          reject(new Error("Cannot add empty object."));
+          return;
+        }
+      }
+
+      // 创建操作事务
+      const transaction = this.db.transaction(storeName, "readwrite");
+      const objectStore = transaction.objectStore(storeName);
+
+      try {
+        // 插入数据
+        Array.isArray(data)
+          ? data.forEach((dataItem) => objectStore.add(dataItem))
+          : objectStore.add(data);
+      } catch (error) {
+        reject(error);
+        return;
+      }
+
+      // 监听状态
+      transaction.oncomplete = () => resolve(true);
+      transaction.onerror = (event) =>
+        reject((event.target as IDBTransaction).error || new Error("Transaction failed."));
     });
   }
 }
