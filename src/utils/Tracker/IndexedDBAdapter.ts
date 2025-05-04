18dc80d97cd9535d3d7f210d84d7560a8318ab1d
lucas <stempest@163.com>
2025-05-04 19:17:57 +0800

    chore(IndexDB): update IndexDB methods

--- src/utils/Tracker/IndexedDBAdapter.ts
+++ src/utils/Tracker/IndexedDBAdapter.ts
@@ -5,7 +5,7 @@ export default class IndexedDBAdapter {
     this.dbName = dbName;
   }
 
-  private async initDB(
+  private async initialize(
     storeName: string | string[],
     indexes: string | string[]
   ): Promise<IDBDatabase> {
@@ -33,52 +33,56 @@ export default class IndexedDBAdapter {
         resolve(this.db);
       };
 
-      request.onerror = reject;
+      request.onerror = (event) => (event.target as IDBOpenDBRequest).error;
     });
   }
 
-  private async addData(
+  private async executeTransaction(
     storeName: string,
-    data: Record<string, any> | Record<string, any>[]
-  ): Promise<boolean> {
+    mode: IDBTransactionMode,
+    operation: (store: IDBObjectStore) => void
+  ): Promise<void> {
     return new Promise((resolve, reject) => {
-      // 校验DB对象
       if (!this.db) {
-        reject(new Error("Database is undefined."));
+        reject(new Error("Database not initialized."));
         return;
       }
 
-      // 校验数据格式
-      if (Array.isArray(data)) {
-        if (data.length === 0) {
-          reject(new Error("Cannot add empty array."));
-          return;
-        }
-      } else {
-        if (Object.keys(data).length === 0) {
-          reject(new Error("Cannot add empty object."));
-          return;
-        }
-      }
+      const transaction = this.db.transaction(storeName, mode);
+      const store = transaction.objectStore(storeName);
 
-      // 创建操作事务
-      const transaction = this.db.transaction(storeName, "readwrite");
-      const objectStore = transaction.objectStore(storeName);
+      operation(store);
 
-      try {
-        // 插入数据
-        Array.isArray(data)
-          ? data.forEach((dataItem) => objectStore.add(dataItem))
-          : objectStore.add(data);
-      } catch (error) {
-        reject(error);
-        return;
-      }
-
-      // 监听状态
-      transaction.oncomplete = () => resolve(true);
-      transaction.onerror = (event) =>
-        reject((event.target as IDBTransaction).error || new Error("Transaction failed."));
+      transaction.oncomplete = () => resolve();
+      transaction.onerror = (event) => reject((event.target as IDBRequest).error);
     });
+  }
+
+  async add<T extends Record<string, any>>(storeName: string, items: T | T[]): Promise<void> {
+    // 校验数据格式
+    if (Array.isArray(items)) {
+      if (items.length === 0) {
+        throw new Error("Cannot add empty array.");
+      }
+    } else {
+      if (Object.keys(items).length === 0) {
+        throw new Error("Cannot add empty object.");
+      }
+      items = [items];
+    }
+
+    return this.executeTransaction(storeName, "readwrite", (store) => {
+      items.forEach((dataItem) => store.add(dataItem));
+    });
+  }
+
+  async update<T extends Record<string, any>>(storeName: string, items: T | T[]): Promise<void> {
+    return this.executeTransaction(storeName, "readwrite", (store) => {
+      store.put(items);
+    });
+  }
+
+  async delete(storeName: string, indexes: number | number[]): Promise<void> {
+    return this.executeTransaction(storeName, "readwrite", (store) => store.delete(indexes));
   }
 }
